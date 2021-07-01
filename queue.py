b54ad32f68658dd0602cc462f67c23a8948a82e7
Sergio <sslp@cin.ufpe.br>
2021-07-01 11:26:25 -0300

    car chasing ok, only one bug when restarting

---
+++ queue.py
@@ -0,0 +1,14 @@
+import collections
+
+class Queue:
+    def __init__(self):
+        self.elements = collections.deque()
+    
+    def empty(self):
+        return not self.elements
+    
+    def put(self, x):
+        self.elements.append(x)
+    
+    def get(self):
+        return self.elements.popleft()
